7ac6927a45fc77d2db3943fd30be3528b4fb2dee
Natalia Albornoz <hola.natavica@gmail.com>
2024-11-15 13:35:56 -0300

    refactorización

--- assets/functions.js
+++ assets/functions.js
@@ -5,14 +5,23 @@
 //Para obtener el promedio debo saber cuantas son las notas, sumarlas y dividir por la cantidad de calificaciones.
 
 
-
-function CalcularPromedio(){
+function CalcularSuma(){
     const v1 = document.getElementById("n1").value;
     const v2 = document.getElementById("n2").value;
     const v3 = document.getElementById("n3").value;
     const v4 = document.getElementById("n4").value;
     const v5 = document.getElementById("n5").value;
     const v6 = document.getElementById("n6").value;
-    const pro = (parseFloat(v1)+parseFloat(v2)+parseFloat(v3)+parseFloat(v4)+parseFloat(v5)+parseFloat(v6))/3;
-    document.getElementById("promedio").innerHTML = pro;
-}
+    const suma = (parseFloat(v1)+parseFloat(v2)+parseFloat(v3)+parseFloat(v4)+parseFloat(v5)+parseFloat(v6));
+    const promedio = (suma) / 6;
+
+
+    document.getElementById("suma").innerHTML = suma;
+    document.getElementById("promedio").innerHTML = promedio;
+
+   MostrarInfo();
+}    
+
+function MostrarInfo(){
+    document.getElementById("info").classList.remove('disabled')
+ }
